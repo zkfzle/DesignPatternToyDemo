e25d73884f3432a8669299e6b6125a5a282c85d9
zhouzhnekun <zkfzle@126.com>
2017-10-05 19:15:11 +0800

    A first runable version of this toy demo.

--- Context.java
+++ Context.java
@@ -1,3 +1,42 @@
-public class Context {
+class Context {
+    private State mNonActiveState;
+    private State mActiveState;
+    private State mOffState;
+    private State mCurState;
+    public static final int NON_ACTIVE_STATE = 0;
+    public static final int OFF_STATE = 1;
+    public static final int ACTIVE_STATE = 2;
+
+    public Context() {
+        mNonActiveState = new NonActiveState(this);
+        mOffState = new OffState(this);
+        mActiveState = new ActiveState(this);
+        mCurState = mNonActiveState;
+    }
     
+    public void setCurState(int stateID) {
+        switch(stateID) {
+            case NON_ACTIVE_STATE:
+                mCurState = mNonActiveState;
+                break;
+            case ACTIVE_STATE:
+                mCurState = mActiveState;
+                break;
+            case OFF_STATE:
+                mCurState = mOffState;
+                break;
+            default:
+                mCurState = mNonActiveState;
+        } 
+    }
+    void testFlushStartPattern() { 
+        // flush, start, flush, start, flush, start ->bingo
+        mCurState.flush();
+        mCurState.startApp();
+        mCurState.flush();
+        mCurState.startApp();
+        mCurState.flush();
+        mCurState.startApp();
+        mCurState.screenOff();
+    }
 }
--- Entry.java
+++ Entry.java
@@ -1,17 +1,7 @@
-class Entry {
-    private static State mNonActiveState;
-    private static State mOffState;
-    private Context mCtx = new Context(); 
-    private State mCurState = mNonActveState;
-
-    public Entry() {
-        mNonActiveState = new NonActiveState(mCtx);
-        mOffState = new OffState(mCtx);
-    }
+public class Entry {
     public static void main(String args[]) {
-	    System.out.println("Hello world!");
-        // flush, start, flush, start, flush, start ->bingo
-        
-
+        Context ctx = new Context();
+        ctx.testFlushStartPattern();
+        System.out.println("Hello world!");
     }
 }
